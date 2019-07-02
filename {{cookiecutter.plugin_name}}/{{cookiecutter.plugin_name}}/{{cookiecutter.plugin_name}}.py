269726c47d8c9544286ba6571e67bdecc960fd3c
Edgardo Marchi <edgardo.marchi@gmail.com>
2019-07-02 16:16:12 -0300

    Added UI and example code to the template

--- {{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}.py
@@ -21,10 +21,10 @@ try:
                            '{{cookiecutter.plugin_name}}.json'), 'r') as plFile:
         defaultParameters = json.load(plFile)
 except json.JSONDecodeError as e:
-    defaultParameters = {}
+    pluginDefaultData = {}
     logger.warning('No valid json format! - Exception: {0!s}'.format(e))
 except IOError:
-    defaultParameters = {}
+    pluginDefaultData = {}
     logger.exception('No plugin data structure found!')
 
 
@@ -37,12 +37,21 @@ class {{cookiecutter.plugin_name}}(BasePlugin):
 
     def __init__(self, 
                  parent=None,
-                 parameters=None,
+                 pluginData=None,
                  gui_mode=False):
-        super().__init__(parent, parameters, gui_mode)
+        super().__init__(parent, pluginData, gui_mode)
 
-        if not self._parameters:
-            self._parameters = defaultParameters
+        if not self._pluginData:
+            self._pluginData = pluginDefaultData
+        
+        # GUI (if required)
+        if self._gui_mode:
+            self._widget = QtWidgets.QWidget()
+            self.ui = uic.loadUi(os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), 'ui', '{{cookiecutter.plugin_name}}.ui'),
+                baseinstance=self._widget)
+            self.ui.valueSpinBox.valueChanged.connect(self.setValue)
+            self.ui.pushMeButton.clicked.connect(self.printValue)
 
   
     def cleanUp(self):
@@ -51,10 +60,10 @@ class {{cookiecutter.plugin_name}}(BasePlugin):
         super().cleanUp()
         # TODO: Add your cleanUp code here
 
-    def fromJson(self, parameters: dict):
+    def fromJson(self, pluginData: dict):
         ''' TODO: Place comments about loading data here.
         '''
-        super().fromJson
+        super().fromJson(pluginData)
         # TODO: Add your code here
 
     @BasePlugin._producesChange
@@ -62,3 +71,10 @@ class {{cookiecutter.plugin_name}}(BasePlugin):
         # Example method that changes the plugin state.
         pass
  
+    @BasePlugin._producesChange
+    def setValue(self, value=0):
+        self._pluginData['parameters']['value']=value
+
+    def printValue(self):
+        self.ui.printLabel.setText('Measured value: {0!s}'.format(
+            self._pluginData['parameters']['value']))
